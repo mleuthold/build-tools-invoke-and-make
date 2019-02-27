35055a0f43e58c5ee17a1d8d6a8bcaee6b405dfb
Martin Leuthold <martinleuthold88+git@gmail.com>
2019-02-27 09:09:17 +0100

    Try out calling tasks within tasks in the target called chain.

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -17,10 +17,18 @@ def build(c):
 def deploy(c):
     print("Deploying!")
 
+@task(docs.hello_world, app.build,release.release)
+def chain(c):
+    print("Calling other tasks within a task doesn't work...")
+    # docs.hello_world()
+    # app.build()
+    # release.release()
+
 
 ns = Collection().from_module(type)
 ns.add_task(build)
 ns.add_task(deploy)
+ns.add_task(chain)
 ns.add_task(release.release)
 ns.add_collection(docs)
 ns.add_collection(app)
