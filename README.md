4f8ddc311be6643fd7a24f31fe53baac4d9b39d6
Martin Leuthold <martinleuthold88+git@gmail.com>
2019-03-29 15:29:42 +0100

    Use YQ to support YAML configuration for shell scripts instead of simple properties files.

--- README.md
+++ README.md
@@ -4,17 +4,23 @@ Compare build tools `invoke` and `make`.
 
 The use case is a multi-project setup, i.e. multiple projects are hosted in a single git repo. Each sub project creates it's own Docker image.
 
+# prerequisites
+
+Enable YAML configuration files for `SHELL` scripts:
+
+```bash
+pip install --user yq
+```
+
 # pros and cons
 
 make with Makefile | invoke with tasks
 | --- | --- |
 | + well known standard | + yaml as config files |
-| + direct shell scripts with highlighting in IDE | + Python instead of shell scripting |
-| + less lines of code (2/3) | + direct calling of sub-project tasks |
-| - redundant code in each Makefile for loading properties | + clear separation of config loading from tasks file |
-| - Makefiles are scattered around in each sub-project | . |
+| + shell scripts with highlighting in IDE | + Python instead of shell scripting |
+| + less lines of code (2/3) | . |
 | - Shell scripts need to adopt specific $$ syntax | . |
-| + can run targets in parallel | parallelism not supported |
+| + can run targets in parallel | - parallelism not supported |
 
 # make with Makefile
 
--- mk/project_a.mk
+++ mk/project_a.mk
@@ -1,18 +1,18 @@
 build.project_a:
-	cd project_a; docker build --tag=$(PROJECT_A_TAG) .
+	docker build --tag=$$(yq .project_a.tag configs/$(ENVIRONMENT).yaml -r) project_a
 
 run.project_a:
-	docker run -p $(PROJECT_A_PORT):80 $(PROJECT_A_TAG)
+	docker run -p $$(yq .project_a.port configs/$(ENVIRONMENT).yaml -r):80 $$(yq .project_a.tag configs/$(ENVIRONMENT).yaml -r)
 
 run.detached.project_a:
-	MY_ID=$$(docker container ls | grep $(PROJECT_A_TAG) | awk '{print $$1}'); \
+	MY_ID=$$(docker container ls | grep $$(yq .project_a.tag configs/$(ENVIRONMENT).yaml -r) | awk '{print $$1}'); \
 	echo "MY_ID: $$MY_ID"; \
-	if test -z $$MY_ID; then docker run -d -p $(PROJECT_A_PORT):80 $(PROJECT_A_TAG); else echo "container is already running ..."; fi
+	if test -z $$MY_ID; then docker run -d -p $$(yq .project_a.port configs/$(ENVIRONMENT).yaml -r):80 $$(yq .project_a.tag configs/$(ENVIRONMENT).yaml -r); else echo "container is already running ..."; fi
 
 check.project_a:
-	curl http://localhost:$(PROJECT_A_PORT) | grep "^<h3>Hello World!</h3><b>Hostname:</b> .*<br/><b>Visits:</b> <i>cannot connect to Redis, counter disabled</i>"
+	curl http://localhost:$$(yq .project_a.port configs/$(ENVIRONMENT).yaml -r) | grep "^<h3>Hello World!</h3><b>Hostname:</b> .*<br/><b>Visits:</b> <i>cannot connect to Redis, counter disabled</i>"
 
 stop.project_a:
-	MY_ID=$$(docker container ls | grep $(PROJECT_A_TAG) | awk '{print $$1}'); \
+	MY_ID=$$(docker container ls | grep $$(yq .project_a.tag configs/$(ENVIRONMENT).yaml -r) | awk '{print $$1}'); \
 	echo "MY_ID: $$MY_ID"; \
 	if test ! -z $$MY_ID; then docker container stop $$MY_ID; else echo "container is already gone ..."; fi
