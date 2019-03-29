e7e977c599d3aeb22cbed4800fb557c1d34de0d8
Martin Leuthold <martinleuthold88+git@gmail.com>
2019-03-29 15:42:36 +0100

    Use call function of MAKE to not repeat YQ call.

--- mk/project_a.mk
+++ mk/project_a.mk
@@ -1,18 +1,20 @@
+get_conf = $$(yq $(1) configs/$(ENVIRONMENT).yaml -r)
+
 build.project_a:
-	docker build --tag=$$(yq .project_a.tag configs/$(ENVIRONMENT).yaml -r) project_a
+	docker build --tag=$(call get_conf,.project_a.tag) project_a
 
 run.project_a:
-	docker run -p $$(yq .project_a.port configs/$(ENVIRONMENT).yaml -r):80 $$(yq .project_a.tag configs/$(ENVIRONMENT).yaml -r)
+	docker run -p $(call get_conf,.project_a.port):80 $(call get_conf,.project_a.tag)
 
 run.detached.project_a:
-	MY_ID=$$(docker container ls | grep $$(yq .project_a.tag configs/$(ENVIRONMENT).yaml -r) | awk '{print $$1}'); \
+	MY_ID=$$(docker container ls | grep $(call get_conf,.project_a.tag) | awk '{print $$1}'); \
 	echo "MY_ID: $$MY_ID"; \
-	if test -z $$MY_ID; then docker run -d -p $$(yq .project_a.port configs/$(ENVIRONMENT).yaml -r):80 $$(yq .project_a.tag configs/$(ENVIRONMENT).yaml -r); else echo "container is already running ..."; fi
+	if test -z $$MY_ID; then docker run -d -p $(call get_conf,.project_a.port):80 $(call get_conf,.project_a.tag); else echo "container is already running ..."; fi
 
 check.project_a:
-	curl http://localhost:$$(yq .project_a.port configs/$(ENVIRONMENT).yaml -r) | grep "^<h3>Hello World!</h3><b>Hostname:</b> .*<br/><b>Visits:</b> <i>cannot connect to Redis, counter disabled</i>"
+	curl http://localhost:$(call get_conf,.project_a.port) | grep "^<h3>Hello World!</h3><b>Hostname:</b> .*<br/><b>Visits:</b> <i>cannot connect to Redis, counter disabled</i>"
 
 stop.project_a:
-	MY_ID=$$(docker container ls | grep $$(yq .project_a.tag configs/$(ENVIRONMENT).yaml -r) | awk '{print $$1}'); \
+	MY_ID=$$(docker container ls | grep $(call get_conf,.project_a.tag) | awk '{print $$1}'); \
 	echo "MY_ID: $$MY_ID"; \
 	if test ! -z $$MY_ID; then docker container stop $$MY_ID; else echo "container is already gone ..."; fi
